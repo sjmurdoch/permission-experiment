66db41acdf18af0eb7e90e8e672a7c760cfc4469
Steven Murdoch <s.murdoch@ucl.ac.uk>
2022-08-31 18:11:10 +0100

    Improve debugging output

--- permission-experiment/check-container.sh
+++ permission-experiment/check-container.sh
@@ -5,28 +5,33 @@ GID_PATTERN='^Gid:[[:space:]]+101[[:space:]]+101[[:space:]]+101[[:space:]]+101[[
 GROUP_PATTERN_OK='^Groups:[[:space:]]+101[[:space:]]+1001[[:space:]]*$'
 GROUP_PATTERN_VULN='^Groups:[[:space:]]+1001[[:space:]]*$'
 DEBUG_PATTERN='^(Uid|Gid|Groups):[[:space:]]+'
-STATUS_FILENAME="/02-error.html"
+
+outputhtml()
+{
+    cat /01-head.html $1 /03-debughead.html > /usr/share/nginx/html/index.html
+    egrep $DEBUG_PATTERN /proc/$$/status >> /usr/share/nginx/html/index.html
+    cat /04-debugfoot.html >> /usr/share/nginx/html/index.html
+}
 
 if ! egrep -q $UID_PATTERN /proc/$$/status; then
     echo "### The presence of the negative-group permissions vulnerability could not be checked because an unexpected result was obtained in checking the user ID."
+    outputhtml "02-error.html"
     exit
 fi
 
 if ! egrep -q $GID_PATTERN /proc/$$/status; then
     echo "### The presence of the negative-group permissions vulnerability could not be checked because an unexpected result was obtained in checking the group ID."
+    outputhtml "02-error.html"
     exit
 fi
 
 if egrep -q $GROUP_PATTERN_OK /proc/$$/status; then
     echo "### Your container runtime appears to be not be affected by the negative-group permissions vulnerability because it handles supplementary groups correctly."
-    STATUS_FILENAME="02-ok.html"
+    outputhtml "02-ok.html"
 elif egrep -q $GROUP_PATTERN_VULN /proc/$$/status; then
     echo "###  Your container runtime appears to be affected by the negative-group permissions vulnerability because it does not handle supplementary groups correctly."
-    STATUS_FILENAME="02-fail.html"
+    outputhtml "02-fail.html"
 else
     echo "### The presence of the negative-group permissions vulnerability could not be checked because an unexpected result was obtained in checking the supplementary groups."
+    outputhtml "02-error.html"
 fi
-
-cat /01-head.html $STATUS_FILENAME /03-debughead.html > /usr/share/nginx/html/index.html
-egrep $DEBUG_PATTERN /proc/$$/status >> /usr/share/nginx/html/index.html
-cat /04-debugfoot.html >> /usr/share/nginx/html/index.html 
